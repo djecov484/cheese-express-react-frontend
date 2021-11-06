53c3c9df99fbc9eb7b343fb8e9ff2642f356cfb2
djecov484 <djecov484@gmail.com>
2021-11-06 12:54:16 -0700

    frontend is done

---
+++ src/components/Main.js
@@ -0,0 +1,48 @@
+import { useEffect, useState} from "react"
+import { Route, Switch } from "react-router-dom"
+import Index from "../pages/Index"
+import Show from "../pages/Show"
+
+
+function Main(props){
+    const [cheese, setCheese] = useState(null)
+
+    const URL = "http://localhost:4000/cheese/"
+
+    const getCheese = async () => {
+        const response = await fetch(URL)
+        const data = await response.json()
+        setCheese(data)
+    }
+
+    const createCheese = async (cheese) => {
+        await fetch(URL, {
+            method: "post", headers: {
+                "Content-Type": "application/json",  
+            },
+            body: JSON.stringify(cheese),
+        })
+        getCheese()
+    }
+
+    useEffect(() => getCheese(), [])
+
+    
+    return (
+        <main>
+            <Switch>
+                <Route exact path="/">
+                    <Index  cheese={cheese} createCheese={createCheese}/>
+
+                </Route>
+                <Route path="/cheese/:id" 
+                render={(rp) => (
+                    <Show {...rp} />
+                )} />
+
+            </Switch>
+        </main>
+    )
+  } 
+  
+  export default Main
